a521c1831a8fb47f0e3e5e91ee7a4dcfe9f79f0e
Juan Castillo <juanpablodlcc@gmail.com>
2024-12-04 12:40:33 -0600

    [REM] dependecies

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,20 +1,11 @@
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
-import path from 'node:path';
-import { createRequire } from 'node:module';
-
-import { defineConfig, normalizePath } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
-
-const require = createRequire(import.meta.url);
-
-const pdfjsDistPath = path.dirname(require.resolve('pdfjs-dist/package.json'));
-const cMapsDir = normalizePath(path.join(pdfjsDistPath, 'cmaps'));
 
 const root = resolve(__dirname, './src')
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),],
+  plugins: [react()],
   resolve:{
     alias:{
       definitions: resolve(root, './definitions'),
